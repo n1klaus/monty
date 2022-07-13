551f10f6600dbca5c0c20de91765656f8b09d82c
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-13 22:22:01 +0300

    Add pint.c

--- pint.c
+++ pint.c
@@ -1,13 +1,23 @@
 #include "monty.h"
 /**
- * pint - prints the value at the top of the stack, 
+ * pint - prints the value at the top of the stack,
  * followed by a new line
  * @top : element at the top of the stack
- * @size : number of elements in the stack
- * 
+ * @line : line number in the bytecode file
+ *
  * Return: 0 if exited successfully, otherwise 1
  */
-int pint(stack_t **top, unsigned int size)
+int pint(stack_t **top, unsigned int line)
 {
+	stack_t *temp = NULL;
+
+	temp = *top;
+	if (*top != NULL && line >= 1)
+		dprintf(STD_OUT, temp->n);
+	else
+	{
+		dprintf(STD_ERR, "L%d: can't pint, stack empty", line);
+		return (EXIT_FAILURE);
+	}
 	return (EXIT_SUCCESS);
 }
