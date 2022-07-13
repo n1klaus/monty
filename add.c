795029019367f160ee36a3fc2242cc549abf0a8d
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-13 22:23:03 +0300

    Add add.c

--- add.c
+++ add.c
@@ -2,11 +2,24 @@
 /**
  * add - adds the top two elements of the stack
  * @top : element at the top of the stack
- * @size : number of elements in the stack
- * 
+ * @line : line number in the bytecode file
+ *
  * Return: 0 if exited successfully, otherwise 1
  */
-int add(stack_t **top, unsigned int size)
+int add(stack_t **top, unsigned int line)
 {
+	stack_t *new_top = NULL;
+
+	if ((*top) != NULL && temp->prev != NULL && line >= 1)
+	{
+		new_top = (*top)->prev;
+		new_top->n = new_top->n + (*top)->n;
+		pop(top, line);
+	}
+	else
+	{
+		dprintf(STD_ERR, "L%d: can't add, stack too short", line);
+		return (EXIT_FAILURE);
+	}
 	return (EXIT_SUCCESS);
 }
